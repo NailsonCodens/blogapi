3035307c81e17193fd16dbafa4b915e8d19de1aa
nailson <nailson.ivs@codens.com.br>
2022-10-05 22:31:49 -0300

    feat (app/Post/src/routes): Create route for read all posts
    Add code to access endpoint in API
    BLOGAP-10

--- src/app/Post/src/routes/index.ts
+++ src/app/Post/src/routes/index.ts
@@ -1,7 +1,13 @@
 import { json, Router } from "express";
 import { createPostController } from "../useCase/CreatePost";
 import { updatePostCaseController } from "../useCase/UpdatePost";
+import { readPostController } from "../useCase/ReadPost";
+
 const postRouter = Router();
+
+postRouter.get("/all", (request, response) => {
+  return readPostController.handleAllPost(request, response);
+});
 
 postRouter.post("/create", (request, response) => {
   return createPostController.handle(request, response);
