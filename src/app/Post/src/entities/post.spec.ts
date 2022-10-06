49ea2f2052b5f1dbad759a99b07bd1b3fd0db14d
nailson <nailson.ivs@codens.com.br>
2022-10-05 22:27:04 -0300

    test (app/Posts/src/entities) Change object post for a new post
    BLOGAP-9

--- src/app/Post/src/entities/post.spec.ts
+++ src/app/Post/src/entities/post.spec.ts
@@ -2,21 +2,19 @@ import { test, expect, describe } from "vitest";
 import { Post } from "./Post";
 
 describe("Post tests", () => {
-  test("Create an Post", () => {
+  test("Should be able instanciate", () => {
     const post = new Post({
-      title: "teste",
-      detail: "treste",
-      description: "sadasdsa",
-      category: "teste",
-      content: "teste",
-      metatitle: "aasdd",
-      metadescription: "asdasd",
-      metakeywords: "sadsad",
-      metarobots: "sad",
+      title: "Novo 1",
+      detail: "novo 1",
+      description: "",
+      category: "",
+      content: "",
+      metatitle: "",
+      metadescription: "",
+      metakeywords: "",
+      metarobots: "",
     });
 
     expect(post).toBeInstanceOf(Post);
   });
-
-  test("", () => {});
 });
